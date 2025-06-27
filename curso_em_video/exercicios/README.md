20a5fecc5b9a9bb0499b635cf9a4fe8d58ef768c
G. Kelly <38537574+kelly-u@users.noreply.github.com>
2025-06-27 15:38:54 -0300

    Adicionando os primeiros arquivos e diretórios

---
+++ curso_em_video/exercicios/README.md
@@ -0,0 +1,2 @@
+# ExerciciosPythonCursoEmVideo
+Execução da lista de exercícios do Curso Python do canal do Youtube Curso em vídeo.
---
+++ curso_em_video/exercicios/desafios/desafio3.py
@@ -0,0 +1,6 @@
+# cria um script Python que leia dois números e tenta mostrar a soma entre eles.
+
+numero1 = int(input('Digite o 1º número: '))
+numero2 = int(input('Digite o 2º número: '))
+
+print(f'A soma dos numeros {numero1} e o {numero2} é {numero1 + numero2}')
