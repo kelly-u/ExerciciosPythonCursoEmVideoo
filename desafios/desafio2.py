c05081548dc0c91a58b4bd9cdc0148d569c14f06
G. Kelly <38537574+kelly-u@users.noreply.github.com>
2025-06-27 15:44:50 -0300

    Adicionando os diretórios e arquivos corretos

---
+++ desafios/desafio2.py
@@ -0,0 +1,8 @@
+# cria um script Python que leia o dia, mês e o ano de nascimento de uma
+# pessoa a mostra uma mensagem com a data formatada.
+
+dia = input('Digite o dia do seu nascimento: ')
+mes = input('Digite o mês do seu nascimento: ')
+ano = input('Digite o ano do seu nascimento: ')
+
+print(f'Sua data de nascimento é {dia}/{mes}/{ano}')
