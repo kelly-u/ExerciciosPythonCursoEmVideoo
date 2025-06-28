7d183d927c9e8663b5dac3b0dc220fa4b855ce31
kelly-u <gleicekellymacedodd@gmail.com>
2025-06-28 15:37:52 -0300

    Exepmplo básico de cada operador aritmético

---
+++ praticas/operacoesAritmeticas.py
@@ -0,0 +1,29 @@
+# Operadores aritméticos
+
+# Adição +
+adicao = 5 + 2
+print(f'Adição 5 + 2: {adicao}')
+
+# Subtração -
+subtracao = 5 - 2
+print(f'Subtração 5 - 2: {subtracao}')
+
+# Multiplicação *
+multiplicacao = 5 * 2
+print(f'Multiplicação 5 * 2: {multiplicacao}')
+
+# Divisão /
+divisao = 5 / 2
+print(f'Divisão 5 / 2: {divisao}')
+
+# Potência **
+potencia = 5**2
+print(f'Potência 5 ** 2: {potencia}')
+
+# Divisão inteira //
+divisaoInteira = 5 // 2
+print(f'Divisão Inteira 5 // 2: {divisaoInteira}')
+
+# Resto da Divisão %
+restoDivisao = 5 % 2
+print(f'Resto da divisão 5 % 2: {restoDivisao}')
