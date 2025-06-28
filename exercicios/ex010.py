97949e557e335bfb4fbe2d04585cff571e5b8acb
kelly-u <gleicekellymacedodd@gmail.com>
2025-06-28 18:28:50 -0300

    Atualização do valor do dólar no exercício 010

--- exercicios/ex010.py
+++ exercicios/ex010.py
@@ -1,6 +1,6 @@
 # Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostra quantos dólares ela pode comprar. Considere US$1.00 = R$3.27
 
 dinheiroReal = float(input('Quanto dinheiro em real você tem? R$'))
-dinheiroDolar = dinheiroReal / 3.27
+dinheiroDolar = dinheiroReal / 5.48
 
 print(f'R${dinheiroReal} reais são ${dinheiroDolar:.2f} dólares.')
