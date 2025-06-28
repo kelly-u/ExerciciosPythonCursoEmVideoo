aeaf4dce53f541c483ca39b0987b6515cb37fd42
kelly-u <gleicekellymacedodd@gmail.com>
2025-06-28 15:48:46 -0300

    Adição de operações e dicas ao arquivo.

--- praticas/operacoesAritmeticas.py
+++ praticas/operacoesAritmeticas.py
@@ -19,6 +19,7 @@ print(f'Divisão 5 / 2: {divisao}')
 # Potência **
 potencia = 5**2
 print(f'Potência 5 ** 2: {potencia}')
+print(pow(5,2)) # Função interna de potência (assim, perde a ordem de precedência)
 
 # Divisão inteira //
 divisaoInteira = 5 // 2
@@ -27,3 +28,13 @@ print(f'Divisão Inteira 5 // 2: {divisaoInteira}')
 # Resto da Divisão %
 restoDivisao = 5 % 2
 print(f'Resto da divisão 5 % 2: {restoDivisao}')
+
+# Raiz quadrada
+# Faz a potência de um número por meio (1/2)
+raizQuadrada = int(81**(1/2)) # sem esse int, a resposta é float. Não é obrigatório, só organização.
+print(f'Raiz quadrada de 81 é: {raizQuadrada}')
+
+# Rais cúbica
+# Igual a raiz quadrada, mas a potência é por 1/3
+raizCubica = int(127 ** (1/3)) # sem esse int, a resposta é float. Não é obrigatório, só organização.
+print(f'A raiz cubica de 127 é: {raizCubica}')
