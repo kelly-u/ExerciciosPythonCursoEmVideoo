cba4bb4689a9e262c4dfdef15362fc9f39236466
kelly-u <gleicekellymacedodd@gmail.com>
2025-06-28 18:51:33 -0300

    Atualização do exercício ex014

--- exercicios/ex014.py
+++ exercicios/ex014.py
@@ -1,4 +1,4 @@
-# Escreva um programa que converta uma temperatura digitada em ºC e converta para ºF
+# Escreva um programa que converta uma temperatura digitada em ºC e converta para ºF.
 
 C = float(input('Digite a temperatura em ºC: '))
 F = (C * 1.8) + 32
