8c5195482de4223800ae30aa84b334ace088c381
kelly-u <gleicekellymacedodd@gmail.com>
2025-06-28 18:37:01 -0300

    Atualização da porcentagem no exercício 012

--- exercicios/ex012.py
+++ exercicios/ex012.py
@@ -1,6 +1,6 @@
 # Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 
 preco = float(input('Digite o valor do produto: R$'))
-precoDesconto = preco - (preco * 15/100)
+precoDesconto = preco - (preco * 5/100)
 
 print(f'O valor do produto passou de R${preco} para R${precoDesconto} com 5% de desconto.')
