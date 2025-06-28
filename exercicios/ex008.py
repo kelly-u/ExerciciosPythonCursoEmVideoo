a5ae5590f2b884fc351b5bae11b3004dd18cd283
kelly-u <gleicekellymacedodd@gmail.com>
2025-06-28 18:22:39 -0300

    Atualização do exercício 008

--- exercicios/ex008.py
+++ exercicios/ex008.py
@@ -1,7 +1,11 @@
 # Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
 
 m = int(input('Digite um valor em metros: '))
+km = m / 1000
+hm = m / 100
+dam = m / 10
+dm = m * 10
 cm = m * 100
 mm = m * 1000
 
-print(f'm: {m}, cm: {cm}, mm: {mm}')
+print(f'km: {km} | hm: {hm} | dam: {dam} | m: {m} | dm: {dm} | cm: {cm}, mm: {mm}')
