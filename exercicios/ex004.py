56cf69515b9135bbbee2bc6595849a26d596f565
kelly-u <gleicekellymacedodd@gmail.com>
2025-06-28 14:49:09 -0300

    COlocação de um comentário no ex004

--- exercicios/ex004.py
+++ exercicios/ex004.py
@@ -9,4 +9,4 @@ print(f'É alfabetico? {palavra.isalpha()}')
 print(f'É alfanumérico? {palavra.isalnum()}')
 print(f'Está em maiúscula? {palavra.isupper()}')
 print(f'Está em minúscula? {palavra.islower()}')
-print(f'Está capitalizada? {palavra.istitle()}')
+print(f'Está capitalizada? {palavra.istitle()}') # nem maiúscula, nem minúscula
