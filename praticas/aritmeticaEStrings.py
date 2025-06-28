2cc44cf1be31614c4ec31ebcbe9e90fe7a2221ac
kelly-u <gleicekellymacedodd@gmail.com>
2025-06-28 16:16:50 -0300

    Atualizando o arquivo com mais regras e dicas

--- praticas/aritmeticaEStrings.py
+++ praticas/aritmeticaEStrings.py
@@ -11,7 +11,7 @@ print(palavra3)
 
 ############################################################
 
-print('*' * 40)
+print('\n','*' * 40, '\n')
 
 nome = input('Digite seu nome: ')
 print(f'Olá, {nome:>20}'.upper()) # Alinhado a direita 20 espaços
@@ -20,3 +20,17 @@ print(f'Olá, {nome:^20}'.upper()) # Alinhado no meio de 20 espaços
 print(f'Olá, {nome:*>20}'.upper()) # Alinhado a direita 20 espaços com *
 print(f'Olá, {nome:$<20}'.upper()) # Alinhado a esquerda 20 espaços com $
 print(f'Olá, {nome:#^20}'.upper()) # Alinhado no meio de 20 espaços com #
+
+############################################################
+
+print('\n','*' * 40, '\n')
+
+# Quebrar a linha no meio do print
+print(f'Olá! \n Meu nome é {nome}')
+
+# Continuar na mesma linha, mesmo com 2 prints diferentes
+print('Eu gosto muito da cor amarelo e', end=' ') # Colar espaço dentro do end é bom pq ele dá o espaço de um print pra outro.
+print('eu gostaria muuuuuuito de ganhar flores amarelas! s2')
+
+print('Oi', end='>>>') # O que é colocado dentro do end, ele coloca entre um print e outro.
+print('tudo bem?')
