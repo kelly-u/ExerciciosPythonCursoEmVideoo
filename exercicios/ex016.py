6df74f9f983d2441dc10ca61d1eb7d4347e97a19
kelly-u <gleicekellymacedodd@gmail.com>
2025-06-29 13:40:31 -0300

    Finalização do exercício 016

--- exercicios/ex016.py
+++ exercicios/ex016.py
@@ -1,2 +1,6 @@
 # Crie um programa que leia um número Real quealquer pelo teclado e mostre na tela sua porção inteira.
 # Ex: Digite um número: 6.127 -> "O número 6.127 tem a parte inteira 6."
+
+numero = float(input('Digite um número real: '))
+
+print(f'O número {numero} tem a parte inteira {int(numero)}')
