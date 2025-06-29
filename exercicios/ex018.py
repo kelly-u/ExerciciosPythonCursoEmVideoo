e4e7f32b21021051251167bebd7b9c1d4c339814
kelly-u <gleicekellymacedodd@gmail.com>
2025-06-29 15:19:35 -0300

    Correção do exercício - faltou a conversão de graus para radianos para os cálculos

--- exercicios/ex018.py
+++ exercicios/ex018.py
@@ -1,9 +1,13 @@
 # Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
-
+import math
 from math import sin
 from math import cos
 from math import tan
+from math import radians
 
 angulo = float(input('Digite um ângulo para análise: '))
 
-print(f'O ângulo {angulo}º tem SEN = {sin(angulo):.2f} | CO = {cos(angulo):.2f} | TA = {tan(angulo):.2f}')
+anguloRad = radians(angulo)
+# transformando de graus para radianos
+
+print(f'O ângulo {angulo}º tem SEN = {sin(anguloRad):.2f} | CO = {cos(anguloRad):.2f} | TA = {tan(anguloRad):.2f}')
