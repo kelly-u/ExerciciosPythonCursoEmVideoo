20e89760bfe261cf6c78fda0d3d976da57f7dc1a
kelly-u <gleicekellymacedodd@gmail.com>
2025-06-29 14:42:37 -0300

    Finalização do exercício 019

--- exercicios/ex019.py
+++ exercicios/ex019.py
@@ -1,10 +1,11 @@
 # Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
+
 import random
 
 contador = 1
 alunos = []
 while contador < 5:
-    alunos.append(input(f'Digite o nome do {contador}º aluno: '))
+    alunos.append(input(f'Digite o nome do(a) {contador}º aluno(a): '))
     contador += 1
 
 print(f'O nome do aluno escolhido foi {random.choice(alunos)}')
--- exercicios/ex020.py
+++ exercicios/ex020.py
@@ -1,2 +1,13 @@
 # O mesmo professor do desafio anrterior quer sortear a ordemd e apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+import random
+
+contador = 1
+alunos = []
+
+while contador < 5:
+    alunos.append(input(f'Digite o nome do(a) {contador}º aluno(a): '))
+    contador += 1
+
+random.shuffle(alunos)
+print(f'A nova ordem dos alunos para a apresentação = {alunos}')
 
