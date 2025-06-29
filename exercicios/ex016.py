2b04802b82aef3c6eeb3fd4eb3a78158278d0f45
kelly-u <gleicekellymacedodd@gmail.com>
2025-06-29 15:04:41 -0300

    Inclusão de mais uma forma de resolver o problema

--- exercicios/ex016.py
+++ exercicios/ex016.py
@@ -1,6 +1,9 @@
 # Crie um programa que leia um número Real quealquer pelo teclado e mostre na tela sua porção inteira.
 # Ex: Digite um número: 6.127 -> "O número 6.127 tem a parte inteira 6."
 
+from math import trunc
+
 numero = float(input('Digite um número real: '))
 
+print(f'O número {numero} tem a parte inteira {trunc(numero)}')
 print(f'O número {numero} tem a parte inteira {int(numero)}')
