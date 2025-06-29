b34d8edf0ae845b3964ace0485dad12d2e26c8c3
kelly-u <gleicekellymacedodd@gmail.com>
2025-06-29 15:12:58 -0300

    Adição de mais duas formas para calcular a hipotenusa.

--- exercicios/ex017.py
+++ exercicios/ex017.py
@@ -1,9 +1,16 @@
 # Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo, calcule e mostre o comprimento da hipotenusa.
 
 from math import sqrt
+from math import hypot
 
 catetoOposto = float(input(f'Digite o cateto oposto: '))
 catetoAdjacente = float(input(f'Digite o cateto adjacente: '))
-hipotenusa = sqrt((catetoOposto ** 2) + (catetoAdjacente ** 2))
 
-print(f'O triângulo retângulo com CO = {catetoOposto} e CA = {catetoAdjacente}, tem a HI = {hipotenusa}')
+# Forma 1 de descobrir a hipotenusa
+print(f'O triângulo retângulo com CO = {catetoOposto} e CA = {catetoAdjacente}, tem a HI = {hypot(catetoOposto, catetoAdjacente):.2f}')
+
+# Forma 2 de descobrir a hipotenusa
+print(f'O triângulo retângulo com CO = {catetoOposto} e CA = {catetoAdjacente}, tem a HI = {sqrt((catetoOposto ** 2) + (catetoAdjacente ** 2)):.2f}')
+
+# Forma 3 de descobrir a hipotenusa
+print(f'O triângulo retângulo com CO = {catetoOposto} e CA = {catetoAdjacente}, tem a HI = {(catetoOposto ** 2 + catetoAdjacente ** 2) ** (1/2) :.2f}')
