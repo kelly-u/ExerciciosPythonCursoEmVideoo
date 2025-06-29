1fc3486cfb30799c07e9142359559460beea17c3
kelly-u <gleicekellymacedodd@gmail.com>
2025-06-29 14:58:22 -0300

    Finalização do exercício 021

--- exercicios/ex021.py
+++ exercicios/ex021.py
@@ -1,1 +1,11 @@
 # Faça um programa em Python que abra e reproduza o áudio de um arquivo mp3.
+
+import pygame
+
+pygame.mixer.init()
+pygame.mixer.music.load('C:/Users/gleic/Downloads/Bento e Totó - O Patinho Colorido (Desenho Infantil).mp3')
+pygame.mixer.music.play()
+while pygame.mixer.music.get_busy():
+    continue
+
+pygame.quit()
