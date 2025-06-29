7897e08e8504e9fb8fd1f7a27dac1954459cf21d
kelly-u <gleicekellymacedodd@gmail.com>
2025-06-29 15:32:46 -0300

    Ajustes na importação do random

--- exercicios/ex020.py
+++ exercicios/ex020.py
@@ -1,5 +1,6 @@
 # O mesmo professor do desafio anrterior quer sortear a ordemd e apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
-import random
+
+from random import shuffle
 
 contador = 1
 alunos = []
@@ -8,6 +9,6 @@ while contador < 5:
     alunos.append(input(f'Digite o nome do(a) {contador}º aluno(a): '))
     contador += 1
 
-random.shuffle(alunos)
+shuffle(alunos)
 print(f'A nova ordem dos alunos para a apresentação = {alunos}')
 
