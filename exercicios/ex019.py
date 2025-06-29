1dc07a9a9dc23240bf8c563701eddd5f073c7677
kelly-u <gleicekellymacedodd@gmail.com>
2025-06-29 14:19:57 -0300

    Finalização do exercício 019

--- exercicios/ex019.py
+++ exercicios/ex019.py
@@ -1,1 +1,10 @@
 # Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
+import random
+
+contador = 1
+alunos = []
+while contador < 5:
+    alunos.append(input(f'Digite o nome do {contador}º aluno: '))
+    contador += 1
+
+print(f'O nome do aluno escolhido foi {random.choice(alunos)}')
