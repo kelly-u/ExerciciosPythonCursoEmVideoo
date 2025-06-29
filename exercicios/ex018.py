2dfbb29010f5c894d7cfe8edfd137f8fa86d3543
kelly-u <gleicekellymacedodd@gmail.com>
2025-06-29 14:06:58 -0300

    Finalização do exercício 018

--- exercicios/ex018.py
+++ exercicios/ex018.py
@@ -1,1 +1,9 @@
 # Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
+
+from math import sin
+from math import cos
+from math import tan
+
+angulo = float(input('Digite um ângulo para análise: '))
+
+print(f'O ângulo {angulo}º tem SEN = {sin(angulo):.2f} | CO = {cos(angulo):.2f} | TA = {tan(angulo):.2f}')
