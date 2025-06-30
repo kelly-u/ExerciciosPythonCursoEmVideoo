52c8155de3c7c25c0e3723ac1a98958a6157ef00
kelly-u <gleicekellymacedodd@gmail.com>
2025-06-29 23:30:04 -0300

    Exemplos de manipulação de textos com join

---
+++ praticas/manipulaçãoTextosJuncao.py
@@ -0,0 +1,8 @@
+# Manipulando Textos
+# Python diferencia minúsculas e maiúsculas
+
+frase = ['Curso', 'em', 'Vídeo', 'Python']
+print(frase)
+
+# join - juntar todos os elementos de frase com o separador escolhido: ' ' - ',' - ';'
+print('-'.join(frase)) # Curso-em-Vídeo-Python
