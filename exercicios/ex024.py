245c171f3a5b9f3f4ba735ce46e23e5bbdd77dea
kelly-u <gleicekellymacedodd@gmail.com>
2025-06-30 01:08:01 -0300

    Atualização do exercício 024 - menos código

--- exercicios/ex024.py
+++ exercicios/ex024.py
@@ -1,9 +1,8 @@
 # Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome 'SANTO'.
 
 cidade = input('Digite o nome de uma cidade: ')
-if ((cidade.upper()).find('SANTO')) == 0:
-    resultado = f'Sim! A cidade {cidade} tem "SANTO" no nome!'
-else:
-    resultado = f'Não! A cidade {cidade} tem "SANTO" no nome!'
 
-print(resultado)
+if 'SANTO' in cidade.upper():
+    print(f'Sim! A cidade {cidade} tem "SANTO" no nome!')
+else:
+    print(f'Não! A cidade {cidade} tem "SANTO" no nome!')
