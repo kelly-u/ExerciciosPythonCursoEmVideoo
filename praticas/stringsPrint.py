a13423cdfeda77602bcc7e131de765dfa5b49239
kelly-u <gleicekellymacedodd@gmail.com>
2025-06-29 23:37:19 -0300

    Exemplos de manipulação de textos muito grandes no print

---
+++ praticas/stringsPrint.py
@@ -0,0 +1,13 @@
+# Textos muito grandes no print
+# Basta colocar """ dentro do print e fechar onde acabar. Dependendo das quebras de linha do código, o texto será escrito da mesam forma quando for executado.
+
+print("""Porque um menino nos nasceu, um filho se nos deu,
+e o principado estará sobre os seus ombros, e se chamará o seu nome:
+Maravilhoso, Conselheiro, Deus Forte, Pai da Eternidade, Príncipe da Paz.
+Do aumento deste principado e da paz não haverá fim,
+sobre o trono de Davi e no seu reino,
+para o firmar e o fortificar com juízo e com justiça,
+desde agora e para sempre;
+o zelo do Senhor dos Exércitos fará isto. 
+
+Isaías 9:6,7""")
