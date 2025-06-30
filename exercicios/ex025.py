76dfc84b7c33ecfca65bb3f8e1aeaaf54e3481fd
kelly-u <gleicekellymacedodd@gmail.com>
2025-06-30 01:08:15 -0300

    Finalização do exercício 025

--- exercicios/ex025.py
+++ exercicios/ex025.py
@@ -1,1 +1,8 @@
 # Crie um programa que leia o nome de uma pessoa e diga se ela tem 'SILVA' no nome
+
+nome = input('Digite seu nome: ')
+
+if 'SILVA' in nome.upper():
+    print(f'Sim! {nome} tem "SILVA" no nome!')
+else:
+    print(f'Não! {nome} não tem "SILVA" no nome!')
