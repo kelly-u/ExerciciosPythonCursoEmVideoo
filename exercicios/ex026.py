05b709639ddb639a7e64d9547b3ab00fff8bd10a
kelly-u <gleicekellymacedodd@gmail.com>
2025-06-30 02:07:06 -0300

    AJuste na contagem da posição da letra A

--- exercicios/ex026.py
+++ exercicios/ex026.py
@@ -3,8 +3,8 @@
 # Em que posição ela aparece a primeira vez.
 # Em que posição ela aparece a última vez.
 
-frase =  input('Digite uma frase: ').upper()
+frase =  input('Digite uma frase: ').upper().strip()
 
 print(f'A letra "A" aparece {frase.count('A')} vezes na frase!')
-print(f'A letra "A" aparece a primeira vez na posição {frase.find('A')}!')
-print(f'A letra "A" aparece a última vez na posição {frase.rfind('A')}!')
+print(f'A letra "A" aparece a primeira vez na posição {frase.find('A')+1}!')
+print(f'A letra "A" aparece a última vez na posição {frase.rfind('A')+1}!')
