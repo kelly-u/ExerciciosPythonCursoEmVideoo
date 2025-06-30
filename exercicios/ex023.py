7103c0fcecdbaf4bf30a3142ffa2dfa57d957136
kelly-u <gleicekellymacedodd@gmail.com>
2025-06-30 00:49:33 -0300

    Finalização do exercício 023

--- exercicios/ex023.py
+++ exercicios/ex023.py
@@ -5,4 +5,29 @@
 # Centena: 8
 # Milhar: 1
 
+numeroInt = int(input('Digite um número inteiro entre 0 e 9999: '))
+numero = str(numeroInt)
+numero = numero.split()
 
+if numeroInt >= 0 and numeroInt <= 9:
+    print(f'Unidade: {numero[0][0]}')
+    print('Dezena: 0')
+    print('Centena: 0')
+    print('Milhar: 0')
+elif numeroInt > 9 and numeroInt <= 99:
+    print(f'Unidade: {numero[0][1]}')
+    print(f'Dezena: {numero[0][0]}')
+    print('Centena: 0')
+    print('Milhar: 0')
+elif numeroInt > 99 and numeroInt <= 999:
+    print(f'Unidade: {numero[0][2]}')
+    print(f'Dezena: {numero[0][1]}')
+    print(f'Centena: {numero[0][0]}')
+    print('Milhar: 0')
+elif (numeroInt > 999 and numeroInt <= 9999):
+    print(f'Unidade: {numero[0][3]}')
+    print(f'Dezena: {numero[0][2]}')
+    print(f'Centena: {numero[0][1]}')
+    print(f'Milhar: {numero[0][0]}')
+else:
+    print('Número fora do intervalo solicitado!')
