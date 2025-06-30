a906cc7d262f0534802b6c96f2db19560b0b68c3
kelly-u <gleicekellymacedodd@gmail.com>
2025-06-29 23:26:52 -0300

    Exemplos de manipulação de textos com split

---
+++ praticas/manipulandoTextosDivisao.py
@@ -0,0 +1,8 @@
+# Manipulando Textos
+# Python diferencia minúsculas e maiúsculas
+
+frase = 'Curso em Vídeo Python'
+print(frase)
+
+# split - divisão nas strings considerando seus espaços ou outros delimitadores.
+print(frase.split()) # ['Curso', 'em', 'Vídeo', 'Python']
