7447e89a6326c230caf122c9a10a1b5cd396e30b
kelly-u <gleicekellymacedodd@gmail.com>
2025-06-30 00:59:45 -0300

    Finalização do exercício 024

--- exercicios/ex024.py
+++ exercicios/ex024.py
@@ -1,1 +1,9 @@
 # Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome 'SANTO'.
+
+cidade = input('Digite o nome de uma cidade: ')
+if ((cidade.upper()).find('SANTO')) == 0:
+    resultado = f'Sim! A cidade {cidade} tem "SANTO" no nome!'
+else:
+    resultado = f'Não! A cidade {cidade} tem "SANTO" no nome!'
+
+print(resultado)
