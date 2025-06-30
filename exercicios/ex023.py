79ebe46c52ba729d1f0fa736cdadd585bb8735d1
kelly-u <gleicekellymacedodd@gmail.com>
2025-06-30 01:56:51 -0300

    Atualizando exercício 023

--- exercicios/ex023.py
+++ exercicios/ex023.py
@@ -5,29 +5,14 @@
 # Centena: 8
 # Milhar: 1
 
-numeroInt = int(input('Digite um número inteiro entre 0 e 9999: '))
-numero = str(numeroInt)
-numero = numero.split()
+numero = int(input('Digite um número inteiro entre 0 e 9999: '))
 
-if numeroInt >= 0 and numeroInt <= 9:
-    print(f'Unidade: {numero[0][0]}')
-    print('Dezena: 0')
-    print('Centena: 0')
-    print('Milhar: 0')
-elif numeroInt > 9 and numeroInt <= 99:
-    print(f'Unidade: {numero[0][1]}')
-    print(f'Dezena: {numero[0][0]}')
-    print('Centena: 0')
-    print('Milhar: 0')
-elif numeroInt > 99 and numeroInt <= 999:
-    print(f'Unidade: {numero[0][2]}')
-    print(f'Dezena: {numero[0][1]}')
-    print(f'Centena: {numero[0][0]}')
-    print('Milhar: 0')
-elif (numeroInt > 999 and numeroInt <= 9999):
-    print(f'Unidade: {numero[0][3]}')
-    print(f'Dezena: {numero[0][2]}')
-    print(f'Centena: {numero[0][1]}')
-    print(f'Milhar: {numero[0][0]}')
-else:
-    print('Número fora do intervalo solicitado!')
+unidade = numero // 1 % 10
+dezena = numero // 10 % 10
+centena = numero // 100 % 10
+milhar = numero // 1000 % 10
+
+print(f'UNIDADE: {unidade}')
+print(f'DEZENA: {dezena}')
+print(f'CENTENA: {centena}')
+print(f'MILHAR: {milhar}')
