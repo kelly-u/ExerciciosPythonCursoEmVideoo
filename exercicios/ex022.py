cc1543ca332c765fe7dd503ea4f11b9c12979cf6
kelly-u <gleicekellymacedodd@gmail.com>
2025-06-30 01:51:26 -0300

    Atualizando exercício 022

--- exercicios/ex022.py
+++ exercicios/ex022.py
@@ -3,8 +3,10 @@
 # O nome com todas as letras minúsculas
 # Quantas letras tem ao toodo sem considerar os espaços
 
-nome = input('Digite seu nome: ')
+nome = str(input('Digite seu nome: ')).strip() # Remover espaços desnecessários
 
 print(f'MAIÚSCULO: {nome.upper()}')
 print(f'MINÚSCULO: {nome.lower()}')
-print(f'QUANTIDADE DE LETRAS: {len(nome.replace(' ', ''))}')
+print(f'QUANTIDADE DE LETRAS: {len(nome) - nome.count(" ")}')
+primeiroNome = nome.split()[0]
+print(f'PRIMEIRO NOME: {primeiroNome} COM {len(primeiroNome)} LETRAS')
