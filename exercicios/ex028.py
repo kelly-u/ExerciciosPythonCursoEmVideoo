b0b3626074aa6305b2ae4931ac7966b2ed72e239
kelly-u <gleicekellymacedodd@gmail.com>
2025-07-01 16:19:26 -0300

    Resolução do exercício 028

--- exercicios/ex028.py
+++ exercicios/ex028.py
@@ -2,3 +2,21 @@
 
 # O programa deverá escrever na tela se o usuário venceu ou perdeu.
 
+from random import randint
+
+numeroComputador = randint(1,5)
+print(numeroComputador)
+
+# No Python não tem o Do | While.
+# Mas, posso usar o While True.
+# Que dá a certeza que a instrução vai ser executada pelo menos uma vez.
+# É comum utilizar aliada a um if e um break.
+
+while True:
+    numeroUsuario = int(input('Estou pensando em um número... Você consegue adivinhar? Diga qual é... '))
+
+    if numeroUsuario == numeroComputador:
+        print('Você acertou!! :)')
+        break
+    else:
+        print('Você errou! :(')
