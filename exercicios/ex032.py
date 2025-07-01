5fef085c187070ee89329f953715d2317339b1ec
kelly-u <gleicekellymacedodd@gmail.com>
2025-07-01 17:05:10 -0300

    Resolução do exercício 032

--- exercicios/ex032.py
+++ exercicios/ex032.py
@@ -1,2 +1,10 @@
 # Faça um programa que leia um ano qualquer e mostre se ele é bissexto.
 
+ano = int(input('Digite um ano: '))
+
+if ano % 400 == 0:
+    print(f'O ano {ano} é bissexto!')
+elif ano % 4 == 0:
+    print(f'O ano {ano} é bissexto!')
+else:
+    print(f'O ano {ano} o não é bissexto!')
