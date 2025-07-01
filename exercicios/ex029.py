d652254a6fd73af1ee6fd1d9ab0064fa8e15fb55
kelly-u <gleicekellymacedodd@gmail.com>
2025-07-01 16:24:32 -0300

    Resolução do exercício 029

--- exercicios/ex029.py
+++ exercicios/ex029.py
@@ -2,3 +2,10 @@
 # Se ele ultrapassar 80km/h, mostre uma mensagem dizendo que ele foi multado.
 # A multa deve custar R$7.00 por cada km acima do limite.
 
+velocidadeCarro = float(input('Digite a velocidade do carro: '))
+
+if velocidadeCarro > 80:
+    excessoVelocidade = velocidadeCarro - 80
+    multa = 7.00 * excessoVelocidade
+
+    print(f'Seu carro foi multado em R${multa:.2f} por ter passado {excessoVelocidade}km/h do limite de velocidade de 80km/h')
