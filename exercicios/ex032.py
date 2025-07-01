ea2b8f6b5c6e7a4d50354b376cbcf18302930fe3
kelly-u <gleicekellymacedodd@gmail.com>
2025-07-01 18:54:43 -0300

    Atualização da resolução do exercício 032

--- exercicios/ex032.py
+++ exercicios/ex032.py
@@ -1,10 +1,12 @@
 # Faça um programa que leia um ano qualquer e mostre se ele é bissexto.
 
-ano = int(input('Digite um ano: '))
+from datetime import date
 
-if ano % 400 == 0:
-    print(f'O ano {ano} é bissexto!')
-elif ano % 4 == 0:
+ano = int(input('Digite um ano para análise (caso queira o ano atual, digite 0): '))
+
+if ano == 0:
+    ano = date.today().year # Da data de hoje, vai pegar só o ano
+if ano % 400 == 0 or ano % 4 == 0 and ano % 100 != 0:
     print(f'O ano {ano} é bissexto!')
 else:
-    print(f'O ano {ano} o não é bissexto!')
+    print(f'O ano {ano} não é bissexto!')
