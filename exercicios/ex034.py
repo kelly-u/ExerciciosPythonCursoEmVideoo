3272e5f199b183923b3d56cc04996cda41f09786
kelly-u <gleicekellymacedodd@gmail.com>
2025-07-01 17:46:32 -0300

    Resolução do exercício 034

--- exercicios/ex034.py
+++ exercicios/ex034.py
@@ -4,3 +4,9 @@
 
 # Para os inferiores ou iguais, o aumento é de 15%.
 
+salario = float(input('Digite o seu salário: '))
+
+if salario > 1250:
+    print(f'Seu salário passou por um aumento de 10% e ficou por R${salario * 1.1:.2f}')
+else:
+    print(f'Seu salário passou por um aumento de 15% e ficou por R${salario * 1.15:.2f}')
