d0f7f964604ac46fa09e60a87315e71a1af00066
kelly-u <gleicekellymacedodd@gmail.com>
2025-07-01 16:36:13 -0300

    Resolução do exercício 030

--- exercicios/ex030.py
+++ exercicios/ex030.py
@@ -1,2 +1,8 @@
 # Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
 
+numero = int(input('Digite um número inteiro para análise: '))
+
+if numero % 2 == 0:
+    print('Número par!')
+else:
+    print('Número ímpar!')
