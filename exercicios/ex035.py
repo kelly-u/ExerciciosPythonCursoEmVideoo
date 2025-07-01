f1c4ce9aca86f9e46b5b54c35be9b3b5d26e4bab
kelly-u <gleicekellymacedodd@gmail.com>
2025-07-01 20:02:52 -0300

    Atualização da resolução do exercício 035 com uma forma de resolução menor

--- exercicios/ex035.py
+++ exercicios/ex035.py
@@ -1,16 +1,14 @@
 # Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se eles podem ou não formar um triângulo.
 
-reta1 = int(input('Digite o comprimento da primeira reta: '))
-reta2 = int(input('Digite o comprimento da segunda reta: '))
-reta3 = int(input('Digite o comprimento da terceira reta: '))
+print('-=' * 20)
+print('ANALIZADOR DE TRIÂNGULOS')
+print('-=' * 20)
 
-if reta1 + reta2 > reta3:
-    if reta2 + reta3 > reta1:
-        if reta1 + reta3 > reta2:
-            print('Os ângulos podem formar um triângulo!')
-        else:
-            print('Os ângulos não podem formar um triângulo!')
-    else:
-        print('Os ângulos não podem formar um triângulo!')
+reta1 = float(input('Digite o comprimento da primeira reta: '))
+reta2 = float(input('Digite o comprimento da segunda reta: '))
+reta3 = float(input('Digite o comprimento da terceira reta: '))
+
+if reta1 + reta2 > reta3 and reta2 + reta3 > reta1 and reta1 + reta3 > reta2:
+    print('Os ângulos podem formar um triângulo!')
 else:
     print('Os ângulos não podem formar um triângulo!')
