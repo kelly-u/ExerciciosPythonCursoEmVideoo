84d8f4a2664a0df6068ce298e5dd5d815b947546
kelly-u <gleicekellymacedodd@gmail.com>
2025-07-01 17:31:40 -0300

    Resolução do exercício 033

--- exercicios/ex033.py
+++ exercicios/ex033.py
@@ -1,2 +1,21 @@
 # Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
 
+num1 = int(input('Digite o 1º número: '))
+num2 = int(input('Digite o 2º número: '))
+num3 = int(input('Digite o 3º número: '))
+
+if num1 > num2 and num1 > num3:
+    maior = num1
+elif num2 > num3 and num2 > num1:
+    maior = num2
+else:
+    maior = num3
+
+if num1 < num2 and num1 < num3:
+    menor = num1
+elif num2 < num3 and num2 < num1:
+    menor = num2
+else:
+    menor = num3
+
+print(f'Menor número = {menor} | Maior número = {maior}')
