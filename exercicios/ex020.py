99d47f97f33a46105f3996fcbd1e68149097353d
kelly-u <gleicekellymacedodd@gmail.com>
2025-07-01 16:19:09 -0300

    Ajuste do comentário no exercício 020

--- exercicios/ex020.py
+++ exercicios/ex020.py
@@ -1,4 +1,4 @@
-# O mesmo professor do desafio anrterior quer sortear a ordemd e apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+# O mesmo professor do desafio anrterior quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
 
 from random import shuffle
 
