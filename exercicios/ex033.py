5d478ae7ff58b5c0df9b4b614ce2d2ab1d35c42c
kelly-u <gleicekellymacedodd@gmail.com>
2025-07-01 19:10:53 -0300

    Atualização da resolução do exercício 033

--- exercicios/ex033.py
+++ exercicios/ex033.py
@@ -4,18 +4,18 @@ num1 = int(input('Digite o 1º número: '))
 num2 = int(input('Digite o 2º número: '))
 num3 = int(input('Digite o 3º número: '))
 
-if num1 > num2 and num1 > num3:
-    maior = num1
-elif num2 > num3 and num2 > num1:
+# Verificando o maior
+maior = num1
+if num2 > num3 and num2 > num1:
     maior = num2
-else:
+if num3 > num1 and num3 > num2:
     maior = num3
 
-if num1 < num2 and num1 < num3:
-    menor = num1
-elif num2 < num3 and num2 < num1:
+# Verificando o menor
+menor = num1
+if num2 < num3 and num2 < num1:
     menor = num2
-else:
+if num3 < num1 and num3 < num2:
     menor = num3
 
 print(f'Menor número = {menor} | Maior número = {maior}')
